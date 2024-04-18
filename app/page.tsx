1a87ceaac2f929e146a141a264ce402353ad6f07
jannick <jannickpedersen@live.dk>
2024-04-18 12:56:39 +0200

    nextjs supabase auth image gallery setup with git login

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Uploader from "@/components/Uploader";
+import Link from "next/link";
+import React from "react";
+
+import { FaGithub, FaYoutube } from "react-icons/fa";
+
+export default function page() {
+	return (
+		<div>
+			<Uploader />
+		</div>
+	);
+}
