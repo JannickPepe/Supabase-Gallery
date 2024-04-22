ac2f22678b21caa28c6fb96c5e6a9b7d4aba705f
jannick <jannickpedersen@live.dk>
2024-04-22 12:34:14 +0200

    you can now upload with image description and added policy for posts, read delete n update with auth

--- components/Uploader.tsx
+++ components/Uploader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger, } from "@/components/ui/dialog";
 import Uppy from '@uppy/core';
 import { Dashboard } from '@uppy/react';
@@ -10,22 +10,28 @@ import { Button } from './ui/button';
 import Tus from '@uppy/tus';
 import useUser from '@/app/hook/useUser';
 import { supabaseBrowser } from '@/lib/supabase/browser';
+import { Input } from './ui/input';
+import { toast } from 'sonner';
 
 
 export default function Uploader() {
 
+    // have it global so every function can use it
+    const supabase = supabaseBrowser();
+
+    //
+    const inputRef = useRef() as React.MutableRefObject<HTMLInputElement>
+    
+    // create a hook
+    const { data: user } = useUser();
+
     // for the request policy so we need to make a Header Request Valid
     const onBeforeRequest = async (req: any) => {
-        const supabase = supabaseBrowser();
         const { data } = await supabase.auth.getSession();
     
         // for every section session we go through this header requirement
         req.setHeader("Authorization", `Bearer ${data.session?.access_token}`);
     };
-    
-
-    // create a hook
-    const { data: user } = useUser();
 
     // make uppy to only upload one image at the time and max size 5mb
     const [uppy] = useState(() => new Uppy(
@@ -58,6 +64,16 @@ export default function Uploader() {
         };
     });
 
+    // 
+    uppy.on("upload-success", () => {
+        uppy.cancelAll();
+        if(inputRef.current) {
+            inputRef.current.value = "";
+        }
+
+        document.getElementById("trigger-close")?.click();
+    });
+
     //
     const handleUpload = () => {
 
@@ -68,7 +84,18 @@ export default function Uploader() {
             objectName: user?.id + "/" + randomUUID + "/" + uppy.getFiles()[0].name,
         });
 
-        uppy.upload();
+        uppy.upload().then(async () => {
+            const description = inputRef.current.value
+
+            if(description.trim()) {
+                const { error } = await supabase.from("posts").update({"description": description}).eq("id", randomUUID);
+
+                if(error) {
+                    toast.error("Fail to update descriptions!!");
+                }
+            }
+
+        });
     };
 
 
@@ -87,6 +114,9 @@ export default function Uploader() {
                     </DialogHeader>
                     <div className='space-y-5'>
                         <Dashboard uppy={uppy} hideUploadButton/>
+
+                        <Input placeholder='image description' ref={inputRef} />
+
                         <Button className='w-full' onClick={handleUpload}>Upload</Button>
                     </div>
                 </DialogContent>
--- lib/types/supabase.ts
+++ lib/types/supabase.ts
@@ -6,9 +6,51 @@ export type Json =
   | { [key: string]: Json | undefined }
   | Json[]
 
-export interface Database {
+export type Database = {
   public: {
     Tables: {
+      posts: {
+        Row: {
+          created_at: string
+          description: string | null
+          id: string
+          name: string
+          object_id: string
+          post_by: string
+        }
+        Insert: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          name: string
+          object_id: string
+          post_by: string
+        }
+        Update: {
+          created_at?: string
+          description?: string | null
+          id?: string
+          name?: string
+          object_id?: string
+          post_by?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_posts_object_id_fkey"
+            columns: ["object_id"]
+            isOneToOne: false
+            referencedRelation: "objects"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "public_posts_post_by_fkey"
+            columns: ["post_by"]
+            isOneToOne: false
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       profiles: {
         Row: {
           created_at: string
@@ -38,7 +80,7 @@ export interface Database {
             isOneToOne: true
             referencedRelation: "users"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
     }
@@ -57,14 +99,16 @@ export interface Database {
   }
 }
 
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
 export type Tables<
   PublicTableNameOrOptions extends
-    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
@@ -72,67 +116,67 @@ export type Tables<
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-      Database["public"]["Views"])
-  ? (Database["public"]["Tables"] &
-      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R
-    }
-    ? R
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
     : never
-  : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I
-    }
-    ? I
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U
-    }
-    ? U
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
-    | keyof Database["public"]["Enums"]
+    | keyof PublicSchema["Enums"]
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
+    : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-  : never
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
